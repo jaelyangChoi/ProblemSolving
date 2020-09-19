966372e4776cc60b621b84cde2f9ab64483e1e27
ChoiJaeLyang <cjl2076@naver.com>
2020-09-19 23:19:32 +0900

    stack - 주식가격 개선

--- src/Stack/Q1.java
+++ src/Stack/Q1.java
@@ -3,35 +3,24 @@ package Stack;
 //주식 가격
 
 import java.util.ArrayDeque;
-
+//피드백: 단순 저장 목적이라면 class를 만들지 않고 int[]을 스택에 넣어도 된다
 public class Q1 {
-    static class Stock {
-        int time;
-        int price;
-
-        public Stock(int time, int price) {
-            this.time = time;
-            this.price = price;
-        }
-    }
-
     static public int[] solution(int[] prices) {
         int n = prices.length;
         int[] answer = new int[n];
-        ArrayDeque<Stock> st = new ArrayDeque<>();
+        ArrayDeque<int[]> st = new ArrayDeque<>();
         //스택의 꼭대기에는 가장 가격이 높은 것만 놓는다.
         for (int i = 0; i < n; i++) {
-            Stock stock = new Stock(i, prices[i]);
-            while (!st.isEmpty() && stock.price < st.peekFirst().price) {////가격이 떨어지면 꺼냄
-                Stock s = st.pollFirst();
-                answer[s.time] = i - s.time; //비 감소 구간 시간 기록
+            while (!st.isEmpty() && prices[i] < st.peekFirst()[1]) {////가격이 떨어지면 꺼냄
+                int[] stock = st.pollFirst();
+                answer[stock[0]] = i - stock[0]; //비 감소 구간 시간 기록
             }
-            st.offerFirst(stock);
+            st.offerFirst(new int[]{i,prices[i]});
         }
         //스택 비우기
         while(!st.isEmpty()){
-            Stock s = st.pollFirst();
-            answer[s.time] = n-1-s.time;
+            int[] stock = st.pollFirst();
+            answer[stock[0]] = n-1-stock[0];
         }
         return answer;
     }
