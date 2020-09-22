92f712b89e1bf723c7c59cb76f1a4aa0e18a1f47
ChoiJaeLyang <cjl2076@naver.com>
2020-09-22 22:31:23 +0900

    그리디 - 큰 수 만들기 스택을 활용해 코드 개선

--- src/Greedy/Q3.java
+++ src/Greedy/Q3.java
@@ -1,36 +1,29 @@
 package Greedy;
 
+import java.util.ArrayDeque;
+
+//큰 수 만들기
+//피드백: 이 문제에 효율적인 자료구조가 무엇인지 생각해보자
 public class Q3 {
     static public String solution(String number, int k) {
         //문제를 부분 문제로 쪼개고, 각 단계에서 최선의 선택. 나보다 작은 앞놈을 다 지운다!
-        char[] numbers = number.toCharArray();
-        //k개를 지울 때 까지
-        int delete = 0;
-        int idx = 1;
-        while (delete < k && idx < number.length()) {
-            int cur = numbers[idx];
-            for (int i = idx - 1; i >= 0; --i) {
-                if (numbers[i] == '#') continue;
-                if (numbers[i] < cur) {
-                    numbers[i] = '#';
-                    ++delete;
-                } else break;
-                if (delete == k) break;
-            }
-            ++idx;
+        char[] result = new char[number.length() - k];
+        //앞놈과 비교해 큰 수를 유지 -> stack!
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < number.length(); ++i) {
+            char c = number.charAt(i);
+            while (!stack.isEmpty() && stack.peekFirst() < c && k-- > 0) //나보다 작은 앞 놈 뺌. 뺄 때마다 k--;
+                stack.pollFirst();
+            stack.offerFirst(c);
         }
-        int cnt=0;
-        StringBuilder sb = new StringBuilder();
-        for (char c : numbers) {
-            if (c == '#') continue;
-            sb.append(c);
-            ++cnt;
-            if(cnt==number.length()-k) break;
-        }
-        return sb.toString();
+
+        for (int i = 0;i<result.length;++i)
+            result[i] = stack.pollLast();
+        return new String(result);
     }
 
     public static void main(String[] args) {
-        System.out.println(solution("7652321", 1));
+        System.out.println(solution("1000", 2));
     }
 }
