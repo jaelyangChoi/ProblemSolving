488965a5247cad2e1d58c973d1df6de18a2d29bc
ChoiJaeLyang <cjl2076@naver.com>
2020-09-22 20:21:07 +0900

    그리디 알고리즘 이해하기 새 배열을 할당받아 정보를 한데 저장함으로써 코드 개선

--- src/Greedy/Q2.java
+++ src/Greedy/Q2.java
@@ -3,42 +3,34 @@ package Greedy;
 //체육복
 
 //그리디 접근: 왼쪽부터 주고 없으면 오른쪽을 준다는 접근이 그리디가 맞았다. 선택에 우선순위를 두는 것. 그것은 단순히 if() else if()여도 된다.
-//그러나 구현에서 틀렸다. 대체 뭐가 잘못된걸까?
+//피드백: 체육복 정보를 한꺼번에 저장하면 계산하기 쉬울 건데 메모리 아낀다고 안 만들었고 더 복잡해졌다. 메모리가 문제가 아니라 효율성이 문제다..
 public class Q2 {
     static public int solution(int n, int[] lost, int[] reserve) {
-        int lostN = lost.length;
-        //lost 중 체육복을 빌릴 수 있는 놈들을 제거
-        int idx = 0;
-        int[] check = new int[lostN];
-        //1. 자기 자신부터
-        for (int i = 0; i < lostN; ++i) {
-            int l = lost[i];
-            while (idx < reserve.length && reserve[idx] < l) ++idx;
-            if (idx >= reserve.length) break;
-            if (reserve[idx] == l) {
-                lost[i] = -1;
-                reserve[idx] = -1;
-                ++idx;
-                --lostN;
-            }
-        }
-        idx = 0;
-        for (int i=0;i<lost.length;++i) {
-            int l=lost[i];
-            if (l == -1) continue;
-            while (idx < reserve.length && reserve[idx] < l - 1) ++idx;
-            if (idx >= reserve.length) break;
-            //2. 앞 놈부터 소진, 없으면 뒷놈
-            if (reserve[idx] -1 == l || reserve[idx]+1 == l) {
-                lost[i]=-1;
-                --lostN;
-                ++idx;
-            }
-        }
+        int[] clothes = new int[n + 1]; //-1: 없음, 0: 있음, 1: 여벌 있음
+        for (int i = 0; i < lost.length; ++i)
+            clothes[lost[i]] = -1;
+        for (int i = 0; i < reserve.length; ++i)
+            ++clothes[reserve[i]];
 
-        return n - lostN;
+        for (int i = 1; i <= n; ++i) {
+            if (clothes[i] == -1) {//체육복이 없을 때 빌릴 수 있는가?
+                //앞부터 빌림
+                if (clothes[i - 1] == 1) {
+                    --clothes[i - 1];
+                    ++clothes[i];
+                }
+                //앞에 없으면 뒤에서 빌림
+                else if (i + 1 <= n && clothes[i + 1] == 1){
+                    --clothes[i + 1];
+                    ++clothes[i];
+                }
+            }
+        }
+        int cnt=0;
+        for(int c: clothes)
+            if(c==-1) cnt++;
+        return n-cnt;
     }
-
     public static void main(String[] args) {
         System.out.println(solution(5, new int[]{2, 4}, new int[]{3}));
     }
