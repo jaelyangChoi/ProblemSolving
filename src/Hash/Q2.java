0b85972268367560acea68b702ea2ac14ad7a985
ChoiJaeLyang <cjl2076@naver.com>
2020-09-24 17:02:44 +0900

    해시 - 전화번호 목록 해시를 사용한 문자열 비교로 코드 개선

--- src/Hash/Q2.java
+++ src/Hash/Q2.java
@@ -1,14 +1,21 @@
 package Hash;
 
 import java.util.Arrays;
-//startWith이라는 메소드가 있구나!
+
+//문자열 정렬은 시간이 많이 든다.
+//노가다 문자열 비교 작업 -> 해시! 숫자로 비교!
+//해시 방식을 통한 비교: 문자열을 하나의 해시코드 숫자로 만든 뒤 숫자를 비교
 public class Q2 {
     static public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
-        String pre = "a";
-        for (String s : phone_book) {
-            if (s.startsWith(pre)) return false;
-            pre = s;
+        int preHash = phone_book[0].hashCode();
+        int prelen = phone_book[0].length();
+        for (int i = 1; i < phone_book.length; ++i) {
+            String b =phone_book[i];
+            if (prelen < b.length() && b.substring(0, prelen).hashCode() == preHash)
+                return false;
+            prelen = b.length();
+            preHash = b.hashCode();
         }
         return true;
     }
