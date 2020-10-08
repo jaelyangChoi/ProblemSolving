b20b2f8c9bfcbc2c2bba54b0568daea646a89e95
ChoiJaeLyang <cjl2076@naver.com>
2020-10-09 00:00:45 +0900

    완전탐색 - m개 미만의 모든 조합 만들기 중복 검사 용도에 적합한 hashset으로 변경

--- src/BruteForce/Q2_2.java
+++ src/BruteForce/Q2_2.java
@@ -1,17 +1,17 @@
 package BruteForce;
 //소수찾기 더 빠른 버전
-import java.util.HashMap;
+import java.util.HashSet;
 
 //길이 m 미만의 모든 조합을 만들기 위해, 선택할 때마다 저장
-//중복 체크로 해시맵을 활용!(배열로 표현하면 7자리 숫자의 인덱스가 되므로 메모리 크기가 커진다)
+//중복 체크로 해시맵을 활용!(배열로 표현하면 7자리 숫자의 인덱스가 되므로 메모리 크기가 커진다) hashset을 써도 된다.
 //교훈: 다른 풀이에서 얻은 인사이트가 도움이 되는구나!
 public class Q2_2 {
     static int[] nums;
     static int cnt = 0;
     static boolean[] check;
-    static HashMap<Integer, Integer> newNum = new HashMap<>();
+    static HashSet<Integer> newNum = new HashSet<>();
 
-    static boolean isPrime(int n) {
+    static boolean isPrime(int n) {//애초에 짝수 거르고 i+=2씩 증가하는 방법이 더 효율적이다.
         if (n <= 1) return false;
         for (int i = 2; i * i <= n; i++)
             if (n % i == 0) return false;
@@ -19,17 +19,16 @@ public class Q2_2 {
     }
 
     static void go(int num, int idx) {
-        if (!newNum.containsKey(num)) {//새로운 수
-            newNum.put(num, 1);
+        if (!newNum.contains(num)) {//새로운 수
+            newNum.add(num);
             if (isPrime(num)) cnt++;
         }
         if (idx == nums.length) return;//다 썼다.
         for (int i = 0; i < nums.length; i++) {
             if (check[i]) continue;
-            //선택
-            check[i] = true;
+            check[i] = true;//선택
             go(num * 10 + nums[i], idx + 1);
-            check[i] = false;
+            check[i] = false;//선택x
         }
     }
 
