2e60ef8c09b7c5aa49d8b49c28511ba3e3d22baf
Jealyang <jaelyangchoi@gmail.com>
2021-06-05 19:09:55 +0900

    solve: baekjoon 12970 by counting

--- src/etc/BJ12970.java
+++ src/etc/BJ12970.java
@@ -3,34 +3,29 @@ package etc;
 import java.util.Scanner;
 
 public class BJ12970 {
-    static int n, k;
-    static char[] ans;
-    static boolean[][][] check;
-
-    static boolean dfs(int i, int a, int pair) {
-        if (i == n)
-            return pair == k;
-        if (pair > k || check[i][a][pair])
-            return false;
-        check[i][a][pair] = true;
-
-        //A 추가
-        ans[i] = 'A';
-        if (dfs(i + 1, a + 1, pair)) return true;
-        //B 추가
-        ans[i] = 'B';
-        return dfs(i + 1, a, pair + a);
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        k = sc.nextInt();
-        check = new boolean[n][n][k + 1];
-        ans = new char[n];
-        if (dfs(0, 0, 0))
-            System.out.println(new String(ans));
-        else
-            System.out.println(-1);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        for (int b = 0; b <= n; b++) {
+            int a = n - b;
+            if (a * b < k) continue;
+
+            int[] position = new int[b + 1];
+            int left = k;
+            for (int i = 0; i < a; i++) {//a번 선택
+                int add = Math.min(left, b);
+                left -= add;
+                position[add] += 1;
+            }
+            for (int i = b; i >= 0; i--) {
+                for (int j = 0; j < position[i]; j++)
+                    System.out.print("A");
+                if (i != 0)
+                    System.out.print("B");
+            }
+            System.exit(0);
+        }
+        System.out.println(-1);
     }
 }
